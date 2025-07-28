e0fb4cd302ff5093b9d0cad9f88c1f24424cb6b7
onurcelik <onurcelik@narcade.com>
2025-07-28 11:10:35 +0300

    Revision: level data fix and unused variable removed

--- Assets/Scripts/Spot/Spot.cs
+++ Assets/Scripts/Spot/Spot.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Spot : Grid
 {
     public Transform Transform;
-    public Vector3 WorldPosition;
     
     public Spot(int x, int z, bool isFull, Transform transform) : base(x, z, isFull)
     {
--- Assets/Scripts/Spot/SpotInstaller.cs
+++ Assets/Scripts/Spot/SpotInstaller.cs
@@ -1,14 +1,15 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Zenject;
 
 public class SpotInstaller : MonoInstaller
 {
-    [SerializeField] private List<Spot> _holdingSpots;
+    [FormerlySerializedAs("_holdingSpots")] [SerializeField] private List<Spot> _spots;
     
     public override void InstallBindings()
     {
-        Container.Bind<SpotsManager>().AsSingle().WithArguments(_holdingSpots).NonLazy();
+        Container.Bind<SpotsManager>().AsSingle().WithArguments(_spots).NonLazy();
         Container.Bind<LevelSpotEndManager>().AsSingle().NonLazy();
         Container.Bind<SpotsMovementManager>().AsTransient().NonLazy();
     }
